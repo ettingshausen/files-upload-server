26aa58d84dd519b3eff63a56bd8eab3b76aa1f4b
ettingshausen <ettingshausen@outlook.com>
2019-05-31 15:45:42 +0800

    Code comment

--- src/main/java/com/winning/fileuploadserver/config/SwaggerConfig.java
+++ src/main/java/com/winning/fileuploadserver/config/SwaggerConfig.java
@@ -11,7 +11,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
- * @author yang_kang@winning.com.cn
+ * @author ettingshausen
  * @date 2019/5/31 15:08
  */
 @Configuration
