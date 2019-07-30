314604a30614f2bcba9c1e6d63fe02a8fe47737e
ettingshausen <ettingshausen@outlook.com>
2019-07-30 13:57:45 +0800

    Bump spring-boot from 2.1.5 to 2.1.6

--- src/main/java/com/winning/fileuploadserver/config/UploadConfig.java
+++ src/main/java/com/winning/fileuploadserver/config/UploadConfig.java
@@ -3,6 +3,8 @@ package com.winning.fileuploadserver.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 /**
  * @author ettingshausen
  * @date 2019/5/31 17:15
@@ -13,6 +15,8 @@ public class UploadConfig {
 
     private String path;
 
+    private List<String> paths;
+
 
     public String getPath() {
         return path;
@@ -20,5 +24,13 @@ public class UploadConfig {
 
     public void setPath(String path) {
         this.path = path;
+    }
+
+    public List<String> getPaths() {
+        return paths;
+    }
+
+    public void setPaths(List<String> paths) {
+        this.paths = paths;
     }
 }
